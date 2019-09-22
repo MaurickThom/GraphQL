d2456a4e5cd79dfb7cf5b390ba6f0bf76badaa51
MaurickThom <thomtwd@gmail.com>
2019-09-21 23:40:37 -0500

    Graphiql

--- 003/src/index.js
+++ 003/src/index.js
@@ -1,8 +1,20 @@
+// Mocking de datos : Esto es una tecnica la cual consiste
+// en hacer pasar datos como si fueran los verdaderos para realizar el testeo de la app
+
 const express = require('express'),
     app = express(),
-    {buildSchema} = require('graphql')
+    {buildSchema} = require('graphql'),
+    graphqlHtpp = require('express-graphql'),
+    courses = require('./courses')
 
-app.use('port',process.env.PORT || 8080)
+app.set('port',process.env.PORT || 8080)
+
+// middleware
+/**
+ * Este middleware lo que hará es montar un `servidor` en nuestro propio 
+ * servidor
+ */
+
 
 const schema = buildSchema(`
     type Course{
@@ -15,8 +27,22 @@ const schema = buildSchema(`
         getCourseByID(id:Int!):Course!
     }
 `)
+const rootV = {
+    getAllCoures(){
+        return courses
+    }
+}
 
-
+app.use('/graphqlapi',graphqlHtpp({
+    schema,
+    rootValue:{ // dentro de este objeto tiene que ir lo resolvers
+                // los resolvers deben tener el mismo nombre
+                // que nuestro query para que haga match
+        ...rootV
+    },
+    graphiql:true // este es una interfaz grafica que nos permitira
+                    // realizar consultas grapql a nuestro servidor
+}))
 
 app.listen(app.get('port'),err=>{
     if(err) return console.log('Ocurrio un error ',err)

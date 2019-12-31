46281956dd3da60a9cb17386d5ccde9d0c053937
MaurickThom <thomtwd@gmail.com>
2019-12-30 23:34:47 -0500

    Rename file bycript --> bcrypt

--- repaso-general/src/utils/helpers/bcrypt.js
+++ repaso-general/src/utils/helpers/bcrypt.js
@@ -1,11 +1,11 @@
 const { compare,genSalt,hash } = require('bcrypt')
 
-exports const encryptPassword = async password=>{
+export const encryptPassword = async password=>{
     const salt = await genSalt(10)
     const _hash = await hash(password,salt)
     return _hash
 }
-exports const matchPassword = async (password,savePassword)=>{
+export const matchPassword = async (password,savePassword)=>{
     try{
         const result = await compare(password,savePassword)
         return result

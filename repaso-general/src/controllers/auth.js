c139ae19b5fb5d53ed0363642066fe107d26a907
MaurickThom <thomtwd@gmail.com>
2020-01-01 09:57:14 -0500

    Auth Controller

--- repaso-general/src/controllers/auth.js
+++ repaso-general/src/controllers/auth.js
@@ -2,4 +2,32 @@ const express = require('express'),
     passport = require('passport'),
     boom = require('@hapi/boom'),
     jwt = require('jsonwebtoken'),
-    router = express.Router()
+    router = express.Router(),
+    { ENV } = require('./../config/config'),
+    { AUTH_JWT_SECRET } = ENV
+
+router.post('/token',async (req,res,next)=>{
+    passport.authenticate( AUTH_JWT_SECRET ,(err,user)=>{
+        try{
+            if(err || !user)
+                return next(boom.unauthorized())
+            req.login(user,{
+                session:false
+            },
+            async error =>{
+                if(error) return next(error)
+                const payload = {
+                    username:user.username,
+                    email:user.email
+                }
+
+                const token = jwt.sign(payload,AUTH_JWT_SECRET,{
+                    expiresIn:'15m'
+                })
+            }
+            )
+        }catch(err){
+
+        }
+    })(req,res,next)
+})

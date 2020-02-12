fc797fb260216b9f41ccd664f5c8ee6ef779a15a
MaurickThom <thomtwd@gmail.com>
2020-02-12 02:11:13 -0500

    tipe definitions

--- apollo-graphql/server.js
+++ apollo-graphql/server.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server')
 
+const courses = require('./courses')
+
 /* La definicion del schema en apollo utiliando las graphql-tools
     se compone en dos partes
     La defincion de los tipos y la definicion de los resolvers
@@ -20,10 +22,15 @@ const typeDefs = `
         getCourseById(id:ID!):Course
     }
 `
+// definicion de schema
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers: {}
+})
 
 // para correr el servidor , no se utilizará express
 const server = new ApolloServer({
-    schema : null
+    schema : schema
 })
 
 // cuando el servidor este escuchando los request lanzará una promesa

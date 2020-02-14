670738c2d41d4a0dab8e93cf2060686aff000f20
MaurickThom <thomtwd@gmail.com>
2020-02-14 18:17:17 -0500

    fixed bug bcrypt

--- graphql-mongo-v2/utils/bcrypt.js
+++ graphql-mongo-v2/utils/bcrypt.js
@@ -4,8 +4,8 @@ const {genSalt,hash,compare} = require('bcrypt')
 
 exports.encryptPassword = async (password)=>{
     const salt = await genSalt(10)
-    const hash = await hash(password,salt)
-    return hash
+    const _hash = await hash(password,salt)
+    return _hash
 }
 
 exports.matchPassword = async (password,savePassword)=>{

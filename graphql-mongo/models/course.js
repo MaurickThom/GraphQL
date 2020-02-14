7fae5ac7726316e25116e695615627bf0f751d1a
MaurickThom <thomtwd@gmail.com>
2020-02-13 20:20:04 -0500

    documentos por referencia

--- graphql-mongo/models/course.js
+++ graphql-mongo/models/course.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
+/**
+ * Relacion de cursos y usuarios (alumno)
+ * 
+ * Un curso lo puede tomar muchos usuarios
+ * Un usuario puede tomar muchos cursos
+ * 
+ * M - M
+ * 
+ * para ello se utilizará documentos en referencia y no embebidos
+ * https://github.com/MaurickThom/MongoDB/blob/master/INSTAGRAM.md
+ */
+
 const courseSchema = new Schema({
     title:String,
     views:Number
